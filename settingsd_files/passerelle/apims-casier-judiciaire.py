1dcc170f55102db82a2e3d549c481dd95a7bb348
Daniel Muyshond <daniel.muyshond@imio.be>
2023-06-15 09:36:26 +0200

    [TELE-1736] Add apims-casier-judiciaire

---
+++ settingsd_files/passerelle/apims-casier-judiciaire.py
@@ -0,0 +1,3 @@
+if 'passerelle_imio_apims_casier_judiciaire' not in INSTALLED_APPS:
+	INSTALLED_APPS += ('passerelle_imio_apims_casier_judiciaire',)
+	TENANT_APPS += ('passerelle_imio_apims_casier_judiciaire',)
